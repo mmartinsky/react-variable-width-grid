7d29029fecccbee93d2eb978a9e90ffc64815497
mmartinsky <michaelmartinsky@yahoo.com>
2024-09-03 22:55:10 -0400

    update deps, use local instance for dev

--- examples/nextjs-example/next.config.js
+++ examples/nextjs-example/next.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     externalDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['react'] = path.resolve(__dirname, '../../node_modules/react');
+    return config;
   },
 }
 
